912750a0f862c78f5b452dd4620ef8d1b235347d
Abdelrahman Mahmoud <abdelrahman.mahmoudd2@gmail.com>
2021-09-10 14:02:39 +0200

    first version Todo App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,91 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <p class="text-center">Todo App</p>
+    <form @submit.prevent="addTodo">
+      <div class="mb-3">
+        <label for="exampleTodo" class="form-label">New Todo</label>
+        <input
+          type="text"
+          class="form-control"
+          id="exampleTodo"
+          aria-describedby="newTodoHelp"
+          v-model="newTodo"
+        />
+        <div id="newTodoHelp" class="form-text">Ender a new todo</div>
+      </div>
+      <button type="submit" class="btn btn-primary">Add Todo</button>
+    </form>
+
+    <ul class="list-group mt-3">
+      <li
+        v-for="(todo,i) in todos"
+        :class="['list-group-item', { done: todo.completed }]"
+        :key="todo"
+      >
+        <span class="float-start ">{{ todo.title }} </span>
+        <button
+          v-if="!todo.completed"
+          class="btn btn-primary btn-sm float-end ms-1"
+          @click="markDone(todo)"
+        >
+          done
+        </button>
+        <button
+          class="btn btn-danger btn-sm float-end"
+          @click="deleteTodo(i)"
+        >
+          delete
+        </button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      newTodo: null,
+      todos: [],
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        completed: false,
+      });
+      this.newTodo = "";
+  
+    },
+    markDone(todo) {
+      todo.completed = !todo.completed;
+    },
+    deleteTodo(i) {
+
+        this.todos.splice(i, 1);
+
+    },
+
+  }, 
+  watch: {
+      todos:{
+          handler() {
+              localStorage.todos =  JSON.stringify(this.todos)
+        },
+        deep : true
+      }
+  },
+  mounted() {
+      if(localStorage.todos){
+          this.todos = JSON.parse(localStorage.todos)
+      }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.done {
+  background: #f03;
 }
 </style>
